b1e17a5b712fe67209a25e9656dd8e8bf5cc20c4
andrew <aboyle3100@gmail.com>
2019-09-25 00:40:14 -0700

    added translate and a bunch of other stuff. still working on grow

--- KrabbyMobile.java
+++ KrabbyMobile.java
@@ -29,58 +29,112 @@ public class KrabbyMobile
 	private Ellipse seed8;
 	private Ellipse seed8L;
 	
+	//wheels
 	private Ellipse w1;
 	private Ellipse w1L;
 	private Ellipse w2;
 	private Ellipse w2L;
 	
+	//colors of the pieces of the burger
 	private Color BROWN;
 	private Color SEED;
 	private Color PURPLE;
-	 
+	
+	//dimensions of krabby mobile
+	private double xx;
+	private double yy;
+	private double w;
+	private double h;
+	private Rectangle bound;
+	
 	public KrabbyMobile(double x, double y){
-		  lettuce = new Rectangle(75,175,450,15);
+		  //x starts at 100 goes off of the bun
+		  //y starts at 100
+		  
+		  xx = x;
+		  yy = y;
+		  
+		  w = 400;
+		  h = 250;
+		  
+		  bound = new Rectangle(xx,yy,w,h+40);
+		  bound.draw();
+		  
+		  lettuce = new Rectangle(xx-25,yy+75,w+50,h-235);
 		
-		  tomato = new Ellipse(75,190,450,25);
+		  tomato = new Ellipse(xx-25,yy+90,w+50,h-225);
 		
-		  onion = new Ellipse(75,215,450,25);
-		  onionlayer = new Ellipse(75,215,450,25);
-		  oL2 = new Ellipse(50,215,430,25);
+		  onion = new Ellipse(xx-25,yy+115,w+50,h-225);
+		  onionlayer = new Ellipse(xx-25,yy+115,w+50,h-225);
+		  oL2 = new Ellipse(xx-50,yy+115,w+30,h-225);
 		
-		  cheese = new Rectangle(75,240,450,10);
-		  cL = new Rectangle(75,240,450,10);
+		  cheese = new Rectangle(xx-25,yy+140,w+50,h-240);
+		  cL = new Rectangle(xx-25,yy+140,w+50,h-225);
 		
-		  patty = new Ellipse(75,250,450,50);
+		  patty = new Ellipse(xx-25,yy+150,w+50,h-200);
 		
-		  bun = new Ellipse(100,100,400,250);
+		  bun = new Ellipse(xx,yy,w,h);
 		
-		  seed1 = new Ellipse(195,125,10,7);
-		  seed1L = new Ellipse(195,125,10,7);
-		  seed2 = new Ellipse(225,135,10,7);
-		  seed2L = new Ellipse(225,135,10,7);
-		  seed3 = new Ellipse(255,125,10,7);
-		  seed3L = new Ellipse(255,125,10,7);
-		  seed4 = new Ellipse(285,135,10,7);
-		  seed4L = new Ellipse(285,135,10,7);
-		  seed5 = new Ellipse(305,125,10,7);
-		  seed5L = new Ellipse(305,125,10,7);
-		  seed6 = new Ellipse(335,135,10,7);
-		  seed6L = new Ellipse(335,135,10,7);
-		  seed7 = new Ellipse(365,125,10,7);
-		  seed7L = new Ellipse(365,125,10,7);
-		  seed8 = new Ellipse(395,135,10,7);
-		  seed8L = new Ellipse(395,135,10,7);
+		  seed1 = new Ellipse(xx+95,yy+25,w-390,h-243);
+		  seed1L = new Ellipse(xx+95,yy+25,w-390,h-243);
+		  seed2 = new Ellipse(xx+125,yy+35,w-390,h-243);
+		  seed2L = new Ellipse(xx+125,yy+35,w-390,h-243);
+		  seed3 = new Ellipse(xx+155,yy+25,w-390,h-243);
+		  seed3L = new Ellipse(xx+155,yy+25,w-390,h-243);
+		  seed4 = new Ellipse(xx+185,yy+35,w-390,h-243);
+		  seed4L = new Ellipse(xx+185,yy+35,w-390,h-243);
+		  seed5 = new Ellipse(xx+205,yy+25,w-390,h-243);
+		  seed5L = new Ellipse(xx+205,yy+25,w-390,h-243);
+		  seed6 = new Ellipse(xx+235,yy+35,w-390,h-243);
+		  seed6L = new Ellipse(xx+235,yy+35,w-390,h-243);
+		  seed7 = new Ellipse(xx+265,yy+25,w-390,h-243);
+		  seed7L = new Ellipse(xx+265,yy+25,w-390,h-243);
+		  seed8 = new Ellipse(xx+295,yy+35,w-390,h-243);
+		  seed8L = new Ellipse(xx+295,yy+35,w-390,h-243);
 		
-		  w1 = new Ellipse(330,340,50,50);
-		  w1L= new Ellipse(330,340,50,50);
-		  w2 = new Ellipse(220,340,50,50);
-		  w2L = new Ellipse(220,340,50,50);
+		  w1 = new Ellipse(xx+230,yy+240,w-350,h-200);
+		  w1L= new Ellipse(xx+230,yy+240,w-350,h-200);
+		  w2 = new Ellipse(xx+120,yy+240,w-350,h-200);
+		  w2L = new Ellipse(xx+120,yy+240,w-350,h-200);
 		 
 		 BROWN = new Color(117, 64, 18);
 		 SEED = new Color(255, 246, 161);
 		 PURPLE = new Color(222, 168, 247);
 	}
 	
+	public void translate(double xxx, double yyy){
+		  bound.translate(xxx,yyy);
+		  lettuce.translate(xxx,yyy);
+		  tomato.translate(xxx,yyy);
+		  onion.translate(xxx,yyy);
+		  onionlayer.translate(xxx,yyy);
+		  oL2.translate(xxx,yyy);
+		  cheese.translate(xxx,yyy);
+		  cL.translate(xxx,yyy);
+		  patty.translate(xxx,yyy);
+		  bun.translate(xxx,yyy);
+		  seed1.translate(xxx,yyy);
+		  seed1L.translate(xxx,yyy);
+		  seed2.translate(xxx,yyy);
+		  seed2L.translate(xxx,yyy);
+		  seed3.translate(xxx,yyy);
+		  seed3L.translate(xxx,yyy);
+		  seed4.translate(xxx,yyy);
+		  seed4L.translate(xxx,yyy);
+		  seed5.translate(xxx,yyy);
+		  seed5L.translate(xxx,yyy);
+		  seed6.translate(xxx,yyy);
+		  seed6L.translate(xxx,yyy);
+		  seed7.translate(xxx,yyy);
+		  seed7L.translate(xxx,yyy);
+		  seed8.translate(xxx,yyy);
+		  seed8L.translate(xxx,yyy);
+		  w1.translate(xxx,yyy);
+		  w1L.translate(xxx,yyy);
+		  w2.translate(xxx,yyy);
+		  w2L.translate(xxx,yyy);
+	}
+
 	public void fill(){
 		
 		
@@ -138,107 +192,12 @@ public class KrabbyMobile
 		w2L.draw();
 	}
 	
-	
-		 // Rectangle lettuce = new Rectangle(75,175,450,15);
+	public void grow(double ww, double hh){
+		//grows by a factor of ww and hh
+		w = w*ww;
+		h = h*hh;
+	}
 		
-		 // Ellipse tomato = new Ellipse(75,190,450,25);
-		
-		 // Ellipse onion = new Ellipse(75,215,450,25);
-		 // Ellipse onionlayer = new Ellipse(75,215,450,25);
-		 // Ellipse oL2 = new Ellipse(50,215,430,25);
-		
-		 // Rectangle cheese = new Rectangle(75,240,450,10);
-		 // Rectangle cL = new Rectangle(75,240,450,10);
-		
-		 // Ellipse patty = new Ellipse(75,250,450,50);
-		
-		 // Ellipse bun = new Ellipse(100,100,400,250);
-		
-		 // Ellipse seed1 = new Ellipse(195,125,10,7);
-		 // Ellipse seed1L = new Ellipse(195,125,10,7);
-		 // Ellipse seed2 = new Ellipse(225,135,10,7);
-		 // Ellipse seed2L = new Ellipse(225,135,10,7);
-		 // Ellipse seed3 = new Ellipse(255,125,10,7);
-		 // Ellipse seed3L = new Ellipse(255,125,10,7);
-		 // Ellipse seed4 = new Ellipse(285,135,10,7);
-		 // Ellipse seed4L = new Ellipse(285,135,10,7);
-		 // Ellipse seed5 = new Ellipse(305,125,10,7);
-		 // Ellipse seed5L = new Ellipse(305,125,10,7);
-		 // Ellipse seed6 = new Ellipse(335,135,10,7);
-		 // Ellipse seed6L = new Ellipse(335,135,10,7);
-		 // Ellipse seed7 = new Ellipse(365,125,10,7);
-		 // Ellipse seed7L = new Ellipse(365,125,10,7);
-		 // Ellipse seed8 = new Ellipse(395,135,10,7);
-		 // Ellipse seed8L = new Ellipse(395,135,10,7);
-		
-		 // Ellipse w1 = new Ellipse(330,340,50,50);
-		 // Ellipse w1L= new Ellipse(330,340,50,50);
-		 // Ellipse w2 = new Ellipse(220,340,50,50);
-		 // Ellipse w2L = new Ellipse(220,340,50,50);
-        // public static void main(String args[])
-        // {
-			// following line is necessary for onMouseClick, don't change
-			// MouseController mC = new MouseController(Canvas.getInstance(),new starter());
-			
-			// please leave following line alone, necessary for keyboard input
-			// KeyController kC = new KeyController(Canvas.getInstance(),new starter());
-		// Color BROWN = new Color(117, 64, 18);
-		// Color SEED = new Color(255, 246, 161);
-		// Color PURPLE = new Color(222, 168, 247);
-		
-		// bun.fill();
-		// bun.setColor(Color.ORANGE);
-		
-		// seed1.fill();
-		// seed1.setColor(SEED);
-		// seed1L.draw();
-		// seed2.fill();
-		// seed2.setColor(SEED);
-		// seed2L.draw();
-		// seed3.fill();
-		// seed3.setColor(SEED);
-		// seed3L.draw();
-		// seed4.fill();
-		// seed4.setColor(SEED);
-		// seed4L.draw();
-		// seed5.fill();
-		// seed5.setColor(SEED);
-		// seed5L.draw();
-		// seed6.fill();
-		// seed6.setColor(SEED);
-		// seed6L.draw();
-		// seed7.fill();
-		// seed7.setColor(SEED);
-		// seed7L.draw();
-		// seed8.fill();
-		// seed8.setColor(SEED);
-		// seed8L.draw();
-		
-		// lettuce.fill();
-		// lettuce.setColor(Color.GREEN);
-		
-		// tomato.fill();
-		// tomato.setColor(Color.RED);
-		
-		// onion.fill();
-		// onion.setColor(PURPLE);
-		// onionlayer.draw();
-		// oL2.setColor(PURPLE);
-		
-		// cheese.fill();
-		// cheese.setColor(Color.YELLOW);
-		// cL.draw();
-		
-		// patty.fill();
-		// patty.setColor(BROWN);
-		
-		// w1.fill();
-		// w1.setColor(Color.GREEN);
-		// w1L.draw();
-		// w2.fill();
-		// w2.setColor(Color.GREEN);
-		// w2L.draw();
-		// }
 		
 		
 }
--- starter.java
+++ starter.java
@@ -10,58 +10,20 @@ public class starter implements InputControl, InputKeyControl
 			// please leave following line alone, necessary for keyboard input
 			KeyController kC = new KeyController(Canvas.getInstance(),new starter());
 		
-			KrabbyMobile kMob = new KrabbyMobile(10,10);
+			KrabbyMobile kMob = new KrabbyMobile(0,0);
 			kMob.fill();
+			kMob.translate(90,90);
+			
 		}
 		
 		public void onMouseClick(double x, double y)
 			{
 				
-			// bun.translate(5,0);
 			
-			// seed1.translate(5,0);
-			// seed1L.translate(5,0);
-			// seed2.translate(5,0);
-			// seed2L.translate(5,0);
-			// seed3.translate(5,0);
-			// seed3L.translate(5,0);
-			// seed4.translate(5,0);
-			// seed4L.translate(5,0);
-			// seed5.translate(5,0);
-			// seed5L.translate(5,0);
-			// seed6.translate(5,0);
-			// seed6L.translate(5,0);
-			// seed7.translate(5,0);
-			// seed7L.translate(5,0);
-			// seed8.translate(5,0);
-			// seed8L.translate(5,0);
-			
-			// w1.translate(5,0);
-			// w1L.translate(5,0);
-			// w2.translate(5,0);
-			// w2L.translate(5,0);
-			
-			// lettuce.translate(5,0);
-			
-			// tomato.translate(5,0);
-			
-			// onion.translate(5,0);
-			
-			// oL2.translate(5,0);
-			// onionlayer.translate(5,0);
-			
-			// cheese.translate(5,0);
-			// cL.translate(5,0);
-			
-			// patty.translate(5,0);
 			}
 		
 		public void keyPress(String s)
-				{
-				// temp holds the enter character
-			
-				char done = (char)10;
-				String temp = Character.toString(done);
-			
-				}
+			{
+				
+			}
 }
